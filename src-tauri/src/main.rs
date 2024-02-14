fca978cc74c8ec001624fa010afa727f1bb039ba
alex <alexfullstackdevops@gmail.com>
2024-02-14 11:48:24 -0300

    final version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,32 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use rand::Rng;
+use std::cmp::Ordering;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+static mut NUMBER_SECRET: Option<i32> = None;
+static mut NUMBER_GENERATED: bool = false;
 
 #[tauri::command]
 fn greet(palpite: &str) -> String {
-    format!("{palpite}!, esse foi seu Palpite.")
+    let number: i32 = palpite.parse().expect("Erro ao converter palpite");
+
+    unsafe {
+        if !NUMBER_GENERATED {
+            NUMBER_SECRET = Some(rand::thread_rng().gen_range(0..=100));
+            NUMBER_GENERATED = true;
+        }
+
+        let result = match (NUMBER_SECRET, number.cmp(&NUMBER_SECRET.unwrap())) {
+            (Some(secret), Ordering::Less) if number < secret => "Muito baixo!",
+            (Some(secret), Ordering::Greater) if number > secret => "Muito alto!",
+            (Some(_), Ordering::Equal) => {
+                NUMBER_GENERATED = false; // Reinicia o jogo
+                "Acertou!"
+            }
+            _ => "Número inválido",
+        };
+
+        format!("{result}")
+    }
 }
 
 fn main() {
--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { useToast } from "./components/ui/use-toast";
 
 function App() {
+  const { toast } = useToast();
   const [response, setResponse] = useState("");
-  const [palpite, setPalpite] = useState("");
+  const [palpite, setPalpite] = useState<string | number>(0);
 
   async function greet() {
     setResponse(await invoke("greet", { palpite }));
   }
 
   return (
-    <div className="flex-col justify-center pt-5 bg-gray-950 text-white h-screen cursor-none select-none">
+    <div className="flex-col justify-center pt-8 bg-gray-950 text-white h-screen cursor-none select-none">
       <h1 className="text-center text-xl">Jogo de Advinhação!</h1>
       <div className="flex justify-center pt-5 gap-3">
         <form
@@ -28,14 +30,31 @@ function App() {
             placeholder="Dê seu palpite de 0 a 100."
           />
           <div className="flex justify-end">
-            <Button className="m-2" variant="secondary" type="submit">
+            <Button
+              onClick={() => {
+                toast({
+                  title: "Alex Say: ",
+                  description: response,
+                  variant: `${response == "Acertou!" ? "default" : "destructive"}`,
+                });
+              }}
+              className="m-2"
+              variant="secondary"
+              type="submit"
+            >
               Palpitar
             </Button>
           </div>
         </form>
       </div>
 
-      <p className="text-center text-white ">{response}</p>
+      {/* <p className="text-center text-white "> */}
+      {/*   {response == "Acertou!" ? ( */}
+      {/*     <span className="text-green-500 font-bold">{response}</span> */}
+      {/*   ) : ( */}
+      {/*     <p className="text-red-800">{response}</p> */}
+      {/*   )} */}
+      {/* </p> */}
     </div>
   );
 }
